90a0d19a0047744e2ea7c587dbfd0b9970ef8ff3
konnykau <mihiro.furuya@gmail.com>
2025-12-10 16:16:26 +0900

    initial commit

---
+++ include/nhk_r1_undercarriage/undercarriage.hpp
@@ -0,0 +1,122 @@
+#pragma once
+#include <math.h>
+#include "nhk_r1_undercarriage/fry_lib/vector.hpp"
+#include "nhk_r1_undercarriage/fry_lib/math.hpp"
+#include "robomas_package_2/msg/motor_cmd_array.hpp"
+#include "robomas_package_2/msg/motor_cmd.hpp"
+#include <cstdint>
+
+
+constexpr float cos45 = FRY::sqrt(2)/2;
+// constexpr float cos30 = FRY::sqrt(3)/2;
+// constexpr float sin30 = 0.5;
+
+//きれいなコードにしたかったぁぁぁ....
+
+class motor{//モーターのクラス
+    private:
+    const FRY::vec2d direction;//モーターの向いている方向ベクトル
+    float TARGET_velocity;//TARGET(m/s)
+    rclcpp::Logger logger_;//printfデバッグに使う
+    const float gear_ratio_ = 1.0;//todo ギア比
+    const float wheel_diameter_ = 0.076;//todo ホイール直径(m)
+    float TARGET_rpm;//TARGETをrpmに変換したやつ
+    public:
+    motor(float x,float y)
+    :direction(FRY::vec2d(x,y)),TARGET_velocity(0),logger_(rclcpp::get_logger("motor"))
+    {}//初期化
+    void set_target(float power){
+        this->TARGET_velocity = power;
+    }//TARGETを代入
+    float make_frame()
+    {
+        //rpmに変換して返す
+        this->TARGET_rpm = (this->TARGET_velocity * 60)/(3.14 * this->wheel_diameter_)*this->gear_ratio_;
+        //このコードがあっているかは不明
+        return this->TARGET_rpm;
+    }
+    FRY::vec2d get_vec2d(){
+        return this->direction;
+    }
+
+
+};
+
+enum class motor_mode{disable,velocity,position};//モーターのmode
+
+
+class undercarriage{//足回り全体のクラス
+    private:
+    FRY::vec2d direction;//進みたい方向
+    
+    motor motors[4];
+    uint8_t id_[4] = {1,2,3,4};
+    //四輪オムニ    
+    motor_mode MODE;
+    rclcpp::Logger logger_;
+    public:
+    
+    undercarriage()
+    :direction(FRY::vec2d(0,0)),motors{motor(-cos45,cos45),motor(-cos45,-cos45),motor(cos45,-cos45),motor(cos45,cos45)},logger_(rclcpp::get_logger("undercarriage"))
+    {
+        MODE = motor_mode::disable;
+    }//初期化
+    void set_motor_power(float theta_vel);//4タイヤがうまく回るようにする
+    void set_direction(float x,float y);//行きたい方向を設定
+    std::unique_ptr<robomas_package_2::msg::MotorCmdArray> make_robomas_Frame();//robomas_package_2に送るメッセージを作成
+    void make_mode(motor_mode motor_state);//modeを設定
+    void update(float x,float y,float turn_dir);//他の関数を全部融合させた
+};
+
+inline void undercarriage::set_direction(float x,float y){
+    this->direction.x = x;
+    this->direction.y = y;
+    //多分m/sになる
+}
+
+inline std::unique_ptr<robomas_package_2::msg::MotorCmdArray> undercarriage::make_robomas_Frame(){
+    robomas_package_2::msg::MotorCmdArray TARGET_FRAME;
+    uint8_t i = 0;
+    for(motor m : this->motors){
+        robomas_package_2::msg::MotorCmd cmd;
+        cmd.id = this->id_[i];
+        cmd.mode = 1;
+        if(this->MODE == motor_mode::velocity){
+            cmd.value = m.make_frame();
+        }
+        else{// if(this->MODE == motor_mode::disable)
+            cmd.value = 0;
+        }
+        RCLCPP_INFO(logger_,"motor id:%d, value:%f",cmd.id,cmd.value);
+        TARGET_FRAME.cmds.push_back(cmd);
+        i++;
+    }
+    RCLCPP_INFO(logger_,"-------------------");
+    
+    return std::make_unique<robomas_package_2::msg::MotorCmdArray>(TARGET_FRAME);
+}
+
+
+
+inline void undercarriage::set_motor_power(float theta_vel){
+    //この前段階でtheta_velは半径などとかけてm/sに変換されている想定
+
+    for(motor &m : motors){
+        float power = (this->direction * m.get_vec2d());
+        m.set_target(power + theta_vel);
+    }
+}
+
+inline void undercarriage::make_mode(motor_mode motor_state){
+    this->MODE = motor_state;
+}
+
+inline void undercarriage::update(float x,float y,float turn_dir)
+{
+    this->set_direction(x,y);
+    this->set_motor_power(turn_dir);
+}
+
+
+
+
---
+++ src/undercarriage.cpp
@@ -0,0 +1,93 @@
+#include <sensor_msgs/msg/joy.hpp>
+#include <functional>
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "nhk_r1_undercarriage/undercarriage.hpp"
+#include "robomas_package_2/msg/motor_cmd_array.hpp"
+#include "robomas_package_2/msg/motor_cmd.hpp"
+#include "nhk_r1_undercarriage/msg/velocity_vector.hpp"
+
+using std::placeholders::_1;
+
+constexpr double parallel_velocity = 1.0; //m/s todo
+constexpr double rotation_velocity = 1.0; //rad/s todo
+constexpr double machine_radius = 0.2; //m todo
+
+
+class Undercarriage_Node: public rclcpp::Node
+{
+public:
+  Undercarriage_Node()
+  : Node("nhk_r1_undercarriage")
+  {
+    subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+      "joy", 10, std::bind(&Undercarriage_Node::joy_callback, this, _1));
+    auto_moving_mode_subscription_ = this->create_subscription<nhk_r1_undercarriage::msg::VelocityVector>(
+      "auto_moving_mode_velocity_vector", 10, std::bind(&Undercarriage_Node::auto_moving_mode_callback, this, _1));
+    robomas_pub_ = this->create_publisher<robomas_package_2::msg::MotorCmdArray>("rm_cmd_array", 10);
+
+  }
+  
+
+private:
+  void joy_callback(const sensor_msgs::msg::Joy & msg)
+  {  
+    if(msg.buttons[7]){//startボタン
+      nhk_r1_undercarriage_.make_mode(motor_mode::velocity);
+      
+    }//mode velにする
+    if(msg.buttons[6]){//backボタン
+      nhk_r1_undercarriage_.make_mode(motor_mode::disable);
+    }//mode disにする
+    if(msg.buttons[9]){//左スティック押し込みボタン
+      auto_moving_mode_ = !auto_moving_mode_;
+      if(auto_moving_mode_){
+        RCLCPP_INFO(this->get_logger(),"auto moving mode ON");
+      }
+      else{
+        RCLCPP_INFO(this->get_logger(),"auto moving mode OFF");
+      }
+    }
+///////////////////////////////ここの上がstartボタン、backボタンによるmodeの調整
+///////////////////////////////ここの下から平行移動、回転をするための個々のモーターのターゲットを決めるif文
+
+   
+    if(auto_moving_mode_){
+      return;
+    }
+    float x = -msg.axes[0] * parallel_velocity;//x方向への平行移動速度
+    float y = msg.axes[1] * parallel_velocity;//y方向への平行移動速度
+    float theta_vel = 0.0; //回転速度
+    if(msg.axes[2] == -1){//ZL(left shouldderボタン)
+      theta_vel = rotation_velocity * machine_radius;
+    }//left turn
+    else if(msg.axes[5] == -1){//ZR(right shouldderボタン)
+      theta_vel = -rotation_velocity * machine_radius;
+     }//right turn//平行移動//x軸はjoyの入力時点で反転していた
+    this->nhk_r1_undercarriage_.update(x,y,theta_vel);
+
+    robomas_pub_->publish(this->nhk_r1_undercarriage_.make_robomas_Frame());
+  }
+
+    void auto_moving_mode_callback(const nhk_r1_undercarriage::msg::VelocityVector & msg)
+    {
+      if(!auto_moving_mode_){
+        return;
+      }
+      RCLCPP_INFO(this->get_logger(),"auto moving mode velocity vector received: x:%f, y:%f, theta:%f",msg.x,msg.y,msg.theta);
+    }
+
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+  rclcpp::Subscription<nhk_r1_undercarriage::msg::VelocityVector>::SharedPtr auto_moving_mode_subscription_;
+  rclcpp::Publisher<robomas_package_2::msg::MotorCmdArray>::SharedPtr robomas_pub_;
+  undercarriage nhk_r1_undercarriage_;
+  bool auto_moving_mode_ = false;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Undercarriage_Node>());
+  rclcpp::shutdown();
+  return 0;
+}
